1e578a5ee1bb1dfb3d65e93dd24dc6f5cf477a0d
zil-web <126854462+zil-web@users.noreply.github.com>
2023-05-31 17:25:06 +0700

    membuat class pekerja turunan dari orang

---
+++ solve multiwaris/solve multiwaris.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class orang {
+public:
+	int umur;
+
+	orang(int pUmur) :
+         
+		umur( pUmur) 
+	{
+		cout << "orang dibuat dengan umur" << umur << "\n" << endl;
+	}
+};
+
+class pekerja : virtual public orang {
+public:
+
+	pekerja(int pUmur) :
+		orang(pUmur)
+	{
+		cout << "pekerja dibuat\n" << endl;
+	}
+};
