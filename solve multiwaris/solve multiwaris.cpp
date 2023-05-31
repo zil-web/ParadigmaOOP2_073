5c82164a7dee91758708a012f2ba4f4a303d0f03
zil-web <126854462+zil-web@users.noreply.github.com>
2023-05-31 17:29:54 +0700

    membuat turunan pelara dari public orang

--- solve multiwaris/solve multiwaris.cpp
+++ solve multiwaris/solve multiwaris.cpp
@@ -22,3 +22,11 @@ public:
 		cout << "pekerja dibuat\n" << endl;
 	}
 };
+class pelajar : virtual public orang {
+public:
+	pelajar(int pUmur) :
+		orang(pUmur)
+	{
+		cout << "pelajar dibuat\n" << endl;
+	}
+};
