14617a3d6d4ba165990ef394634f798a29e219fc
zil-web <126854462+zil-web@users.noreply.github.com>
2023-05-31 17:34:36 +0700

    hal ini digunakan jika menggunakkan virtual

--- solve multiwaris/solve multiwaris.cpp
+++ solve multiwaris/solve multiwaris.cpp
@@ -30,3 +30,14 @@ public:
 		cout << "pelajar dibuat\n" << endl;
 	}
 };
+
+class budi : public pekerja, public pelajar {
+public:
+	budi(int pUmur) :
+		pekerja(pUmur),
+		pelajar(pUmur),
+		orang(pUmur) //hal ini digunakan jika menggunakkan virtual
+	{
+		cout << "budi dibuat\n" << endl;
+	}
+};
