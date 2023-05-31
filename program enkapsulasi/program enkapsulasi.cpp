118e9ee2c7dd098ab5c2125e5ac55ae94a61cc1d
zil-web <126854462+zil-web@users.noreply.github.com>
2023-05-31 21:58:02 +0700

    program enkapsulasi

--- program enkapsulasi/program enkapsulasi.cpp
+++ program enkapsulasi/program enkapsulasi.cpp
@@ -1,20 +1,31 @@
-// program enkapsulasi.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class remotelampu {
+private:
+	string saklarNo[10];
+public:
+	void setsaklarNo(int i, string value) {
+		saklarNo[i] = value;
+	}
+	string getsaklarNo(int i) {
+		return saklarNo[i];
+	}
+};
+
+int main() {
+	remotelampu lampurumah;
+
+	lampurumah.setsaklarNo(0,"lampu teras rumah");
+	lampurumah.setsaklarNo(1, "lampu ruang tamu");
+	lampurumah.setsaklarNo(2, "lampu kamar tidur");
+	lampurumah.setsaklarNo(3, "lampu dapur");
+
+	cout << lampurumah.getsaklarNo(0) << endl;
+	cout << lampurumah.getsaklarNo(1) << endl;
+	cout << lampurumah.getsaklarNo(2) << endl;
+	cout << lampurumah.getsaklarNo(3) << endl;
+
+	return 0;
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
